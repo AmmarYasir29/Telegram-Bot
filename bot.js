fc516fa910e8307da30077aef239c8dcb41c93a1
Ammar <ammar@pop-os.localdomain>
2020-12-17 00:29:53 +0300

    Ready to run on heroku

---
+++ bot.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const Bot = require("node-telegram-bot-api");
+const ytdl = require("ytdl-core");
+require("dotenv").config();
+
+const token = process.env.TOKEN;
+let bot;
+if(process.env.NODE_ENV === 'production') {
+  bot = new Bot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+}
+else {
+  bot = new Bot(token, { polling: true });
+}
+
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1]; 
+  bot.sendMessage(chatId, resp);
+});
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Received your message');
+});
+
+bot.onText(/\/v (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const url = match[1]; 
+  const video = ytdl(url, {
+    quality: "lowestvideo",
+  });
+  video.pipe(fs.createWriteStream("video.mp4"));
+  bot.sendMessage(
+    msg.chat.id,
+    "Please wait while download the video- depend on speed of net",
+    { replyToMessage: msg.message_id }
+  );
+  video.on("info", function (info) {
+    bot.sendMessage(msg.chat.id, `The video still downloading !`);
+  });
+  video.on("end", function () {
+    let vid = `${__dirname}/video.mp4`;
+    let stats = fs.statSync(vid);
+    let fileSizeInBytes = stats.size;
+    var fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024).toFixed(2);
+    if (Number(fileSizeInMegabytes) >= 50) {
+      bot.sendMessage(
+        msg.chat.id,
+        `Video is too large to send via HTTP BOT Api, please set up your own BotAPI Server to send via MTProto...`
+      );
+    } else {
+      bot
+        .sendVideo(msg.chat.id, vid, { replyToMessage: msg.message_id })
+        .catch((error) => console.log(error));
+      fs.unlink(vid, (error) => console.log(error));
+    }
+  });
+});
+
+
+module.exports = bot;
---
+++ web.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let packageInfo = require('./package.json');
+
+let app = express();
+
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
+});
+
+let server = app.listen(process.env.PORT, "0.0.0.0", () =>{
+
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log('Web server started at http://%s:%s', host, port);
+});
+
+module.exports = (bot) => {
+  app.post('/' + bot.token, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+};
