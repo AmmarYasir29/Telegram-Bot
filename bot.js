f99650f8092868b11046cd649988c94e10faa679
Ammar <ammar@pop-os.localdomain>
2020-12-17 22:39:39 +0300

    🤓 download voice & video

--- bot.js
+++ bot.js
@@ -15,16 +15,16 @@ else {
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, 'Welcome to best downloader EVER🔥');
 });
 
 bot.onText(/\/v (.+)/, async (msg, match) => {
   const chatId = msg.chat.id;
   const url = match[1]; 
-
+  if(ytdl.validateURL(url)){
   const video = ytdl(url, {quality: "lowest", filter: format => format.hasVideo && format.hasAudio});
 
-  video.pipe(fs.createWriteStream("video.mp4"));
+  video.pipe(fs.createWriteStream("video.mp3"));
   bot.sendMessage(
     msg.chat.id,
     "Please wait while download the video- depend on speed of net",
@@ -34,7 +34,7 @@ bot.onText(/\/v (.+)/, async (msg, match) => {
     bot.sendMessage(msg.chat.id, `The video still downloading !`);
   });
   video.on("end", function () {
-    let vid = `${__dirname}/video.mp4`;
+    let vid = `${__dirname}/video.mp3`;
     let stats = fs.statSync(vid);
     let fileSizeInBytes = stats.size;
     var fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024).toFixed(2);
@@ -50,7 +50,53 @@ bot.onText(/\/v (.+)/, async (msg, match) => {
       fs.unlink(vid, (error) => console.log(error));
     }
   });
+}else bot.sendMessage(
+  msg.chat.id,
+  "Please send YT video",
+  { replyToMessage: msg.message_id });
+
 });
+
+bot.onText(/\/a (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const url = match[1]; 
+  if(ytdl.validateURL(url)){
+  const audio =  ytdl(url, {quality: "lowestaudio"});
+
+  audio.pipe(fs.createWriteStream("audio.mp3"));
+  bot.sendMessage(
+    msg.chat.id,
+    "Please wait while download the audio- depend on speed of net",
+    { replyToMessage: msg.message_id }
+  );
+  audio.on("info", function (info) {
+    bot.sendMessage(msg.chat.id, `The audio still downloading !`);
+  });
+  audio.on("end", function () {
+    let sound = `${__dirname}/audio.mp3`;
+    let stats = fs.statSync(sound);
+    let fileSizeInBytes = stats.size;
+    var fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024).toFixed(2);
+    if (Number(fileSizeInMegabytes) >= 50) {
+      bot.sendMessage(
+        msg.chat.id,
+        `audio is too large to send via HTTP BOT Api, please set up your own BotAPI Server to send via MTProto...`
+      );
+    } else {
+      bot
+        .sendAudio(msg.chat.id, sound, { replyToMessage: msg.message_id })
+        .catch((error) => console.log(error));
+      fs.unlink(sound, (error) => console.log(error));
+    }
+  });
+}else bot.sendMessage(
+  msg.chat.id,
+  "Please send YT video",
+  { replyToMessage: msg.message_id });
+
+});
+
+
 
 
 module.exports = bot;
