b7a4bddcfbdd21f3dbb9b64ecf7a25986ab66493
Ammar <ammar@pop-os.localdomain>
2021-01-12 20:57:55 +0300

    😎 Add TS code

--- bot.ts
+++ bot.ts
@@ -8,7 +8,7 @@ const isUrl = require("is-url");
 require("dotenv").config();
 
 const token = process.env.TOKEN;
-let bot;
+let bot: any;
 if (process.env.NODE_ENV === "production") {
   bot = new Bot(token);
   bot.setWebHook(process.env.HEROKU_URL + bot.token);
@@ -16,18 +16,18 @@ if (process.env.NODE_ENV === "production") {
   bot = new Bot(token, { polling: true });
 }
 
-bot.onText(/\/start/, (msg) => {
+bot.onText(/\/start/, (msg: any) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, "Welcome to best downloader EVER 🔥");
 });
 
-bot.onText(/\/v (.+)/, async (msg, match) => {
+bot.onText(/\/v (.+)/, async (msg: any, match: string) => {
   const chatId = msg.chat.id;
   const url = match[1];
   if (ytdl.validateURL(url)) {
     const video = ytdl(url, {
       quality: "lowest",
-      filter: (format) => format.hasVideo && format.hasAudio,
+      filter: (format: any) => format.hasVideo && format.hasAudio,
     });
 
     video.pipe(fs.createWriteStream("video.mp3"));
@@ -36,14 +36,14 @@ bot.onText(/\/v (.+)/, async (msg, match) => {
       "Please wait while download the video- depend on speed of net",
       { replyToMessage: msg.message_id }
     );
-    video.on("info", function (info) {
+    video.on("info", function (info: any) {
       bot.sendMessage(msg.chat.id, `The video still downloading !`);
     });
     video.on("end", async function () {
       let vid = `${__dirname}/video.mp3`;
       let stats = fs.statSync(vid);
       let fileSizeInBytes = stats.size;
-      var fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024).toFixed(2);
+      let fileSizeInMegabytes = fileSizeInBytes / <any>(1024 * 1024).toFixed(2);
       if (Number(fileSizeInMegabytes) >= 50) {
         bot.sendMessage(
           msg.chat.id,
@@ -52,8 +52,8 @@ bot.onText(/\/v (.+)/, async (msg, match) => {
       } else {
         await bot
           .sendVideo(msg.chat.id, vid, { replyToMessage: msg.message_id })
-          .catch((error) => console.log(error));
-        fs.unlink(vid, (error) => console.log(error));
+          .catch((error: any) => console.log(error));
+        fs.unlink(vid, (error: any) => console.log(error));
       }
     });
   } else
@@ -62,7 +62,7 @@ bot.onText(/\/v (.+)/, async (msg, match) => {
     });
 });
 
-bot.onText(/\/a (.+)/, async (msg, match) => {
+bot.onText(/\/a (.+)/, async (msg: any, match: any) => {
   const chatId = msg.chat.id;
   const url = match[1];
   if (ytdl.validateURL(url)) {
@@ -74,14 +74,14 @@ bot.onText(/\/a (.+)/, async (msg, match) => {
       "Please wait while download the audio- depend on speed of net",
       { replyToMessage: msg.message_id }
     );
-    audio.on("info", function (info) {
+    audio.on("info", function (info: any) {
       bot.sendMessage(msg.chat.id, `The audio still downloading !`);
     });
     audio.on("end", async function () {
       let sound = `${__dirname}/audio.mp3`;
       let stats = fs.statSync(sound);
       let fileSizeInBytes = stats.size;
-      var fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024).toFixed(2);
+      var fileSizeInMegabytes = fileSizeInBytes / <any>(1024 * 1024).toFixed(2);
       if (Number(fileSizeInMegabytes) >= 50) {
         bot.sendMessage(
           msg.chat.id,
@@ -90,8 +90,8 @@ bot.onText(/\/a (.+)/, async (msg, match) => {
       } else {
         await bot
           .sendAudio(msg.chat.id, sound, { replyToMessage: msg.message_id })
-          .catch((error) => console.log(error));
-        fs.unlink(sound, (error) => console.log(error));
+          .catch((error: any) => console.log(error));
+        fs.unlink(sound, (error: any) => console.log(error));
       }
     });
   } else
@@ -100,66 +100,68 @@ bot.onText(/\/a (.+)/, async (msg, match) => {
     });
 });
 
-//TODO: add profile img to download --> const ipp = require("instagram-profile-picture");
-bot.onText(/\/i (.+)/, async (msg, match) => {
+//TODO:  add profile img to download --> const ipp = require("instagram-profile-picture");
+bot.onText(/\/i (.+)/, async (msg: any, match: any) => {
   const chatId = msg.chat.id;
   const url = match[1];
-  if (!isUrl(url) && !url.includes("instagram")) {
+  if (!isUrl(url) || !url.includes("instagram")) {
     bot.sendMessage(msg.chat.id, "Please send INSTAGRAM video OR image 💥", {
       replyToMessage: msg.message_id,
     });
     return;
   }
+  //FIXME: didnt work inside server
   bot.sendMessage(
     msg.chat.id,
-    "🛑 Please wait while download- depend on speed of net",
+    "Please wait while download- depend on speed of net",
     { replyToMessage: msg.message_id }
   );
-  save(url, `${__dirname}`).then(async (res) => {
+  save(url, `${__dirname}`).then(async (res: any) => {
     let x = res.file;
-    if (x.includes("mp4"))
-      await bot
-        .sendVideo(msg.chat.id, x, { replyToMessage: msg.message_id })
-        .catch((error) => console.log(error));
-    else if (
-      x.includes("jpg") ||
-      x.includes("jpeg") ||
-      x.includes("png") ||
-      x.includes("gif")
-    )
-      await bot
-        .sendPhoto(msg.chat.id, x, { replyToMessage: msg.message_id })
-        .catch((error) => console.log(error));
-    fs.unlink(x, (error) => console.log("Error", error));
+    // if (x.includes("mp4"))
+    await bot
+      .sendVideo(msg.chat.id, x, { replyToMessage: msg.message_id })
+      .catch((error: any) => console.log(error));
+    // else if (
+    //   x.includes("jpg") ||
+    //   x.includes("jpeg") ||
+    //   x.includes("png") ||
+    //   x.includes("gif")
+    // )
+    //   await bot
+    //     .sendPhoto(msg.chat.id, x, { replyToMessage: msg.message_id })
+    //     .catch((error) => console.log(error));
+    // fs.unlink(x, (error) => console.log("Error", error));
   });
 });
 
-bot.onText(/\/f (.+)/, async (msg, match) => {
+bot.onText(/\/f (.+)/, async (msg: any, match: any) => {
   const chatId = msg.chat.id;
   const url = match[1];
-  if (isUrl(url) && url.includes("facebook")){
-  facebookdownload.getInfo(url).then((info) => {
-    let name = info.title;
-    let urlVideo = info.download.sd;
-    bot.sendMessage(msg.chat.id, `The ${name} will download ✨`, {
-      replyToMessage: msg.message_id,
-    });
+  if (isUrl(url) || url.includes("facebook")) {
+    facebookdownload.getInfo(url).then((info: any) => {
+      let name = info.title;
+      let urlVideo = info.download.sd;
+      bot.sendMessage(msg.chat.id, `The ${name} will download ✨`, {
+        replyToMessage: msg.message_id,
+      });
 
-    let file = fs.createWriteStream("video.mp4");
-    https.get(urlVideo, function (response) {
-      response.pipe(file);
-      file.on("finish", async function () {
-        let vid = `${__dirname}/video.mp4`;
-        console.log("finish!");
-        await bot
-          .sendVideo(msg.chat.id, vid, { replyToMessage: msg.message_id })
-          .catch((error) => console.log(error));
-        fs.unlink(vid, (error) => console.log(error));
+      let file = fs.createWriteStream("video.mp4");
+      https.get(urlVideo, function (response: any) {
+        response.pipe(file);
+        file.on("finish", async function () {
+          let vid = `${__dirname}/video.mp4`;
+          console.log("finish!");
+          await bot
+            .sendVideo(msg.chat.id, vid, { replyToMessage: msg.message_id })
+            .catch((error: any) => console.log(error));
+          fs.unlink(vid, (error: any) => console.log(error));
+        });
       });
     });
-  });
-}else bot.sendMessage(msg.chat.id, `🛑 Please send FACEBOOK video `, {
-  replyToMessage: msg.message_id,
-});
+  } else
+    bot.sendMessage(msg.chat.id, `🛑 Please send FACEBOOK video `, {
+      replyToMessage: msg.message_id,
+    });
 });
 module.exports = bot;
